f9e8d3434777008690658129875a07ccd728f77b
Achref Gassoumi <achref.gassoumi1@gmail.com>
2018-03-03 20:36:51 +0100

    add project component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { GitUsersComponent } from './git-users/git-users.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitUsersComponent
   ],
   imports: [
     BrowserModule

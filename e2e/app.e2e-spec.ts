d86aa6e2b00cde7b28d75cf982caea6d6aaf9f2b
Achref Gassoumi <achref.gassoumi1@gmail.com>
2018-03-03 20:14:24 +0100

    commit empty projoect

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('angular-delegate-design-pattern-example App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
